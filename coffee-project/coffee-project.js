0e92af65a85a6f641db3a6f7691cd067278e8674
Steve Pflug <stephen@pflug.us>
2023-04-13 11:43:44 -0500

    updated for loop to render coffees in ascending order by id. removed unused comments. updated variable name for coffee container div

--- coffee-project/coffee-project.js
+++ coffee-project/coffee-project.js
@@ -1,14 +1,9 @@
 "use strict"
 
 function renderCoffee(coffee) {
-    // let html = '<tr class="coffee">';
-    // html += '<td>' + coffee.id + '</td>';
-    // html += '<td>' + coffee.name + '</td>';
-    // html += '<td>' + coffee.roast + '</td>';
-    // html += '</tr>';
 
     let html = `
-        <div class="coffee">
+        <div class="coffee" data-coffee-id="${coffee.id}">
             <div class="coffee-header">
                 <div>${coffee.name}</div>
             </div>
@@ -23,14 +18,14 @@ function renderCoffee(coffee) {
 
 function renderCoffees(coffees) {
     let html = '';
-    for(let i = coffees.length - 1; i >= 0; i--) {
+    for(let i = 0; i < coffees.length; i++) {
         html += renderCoffee(coffees[i]);
     }
     return html;
 }
 
 function updateCoffees(e) {
-    e.preventDefault(); // don't submit the form, we just want to update the data
+    e.preventDefault();
     let selectedRoast = roastSelection.value;
     let filteredCoffees = [];
     coffees.forEach(function(coffee) {
@@ -38,7 +33,7 @@ function updateCoffees(e) {
             filteredCoffees.push(coffee);
         }
     });
-    tbody.innerHTML = renderCoffees(filteredCoffees);
+    coffeesContainer.innerHTML = renderCoffees(filteredCoffees);
 }
 
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
@@ -59,10 +54,10 @@ let coffees = [
     {id: 14, name: 'French', roast: 'dark'},
 ];
 
-let tbody = document.querySelector('#coffees');
+let coffeesContainer = document.querySelector('#coffees');
 let submitButton = document.querySelector('#submit');
 let roastSelection = document.querySelector('#roast-selection');
 
-tbody.innerHTML = renderCoffees(coffees);
+coffeesContainer.innerHTML = renderCoffees(coffees);
 
 submitButton.addEventListener('click', updateCoffees);
